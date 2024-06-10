0c727f5ad50d960526bd19127ef59dbf9ab7c239
Matt Stauffer <matt@tighten.co>
2024-06-10 13:03:02 -0400

    Build out basic seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,46 @@
 
 namespace Database\Seeders;
 
+use App\Models\Organization;
+use App\Models\Site;
+use App\Models\Technology;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $otherUser = User::factory()->create();
+
+        Technology::factory()->count(5)->create();
+
+        $orgs = Organization::factory()->count(2)->create([
+            'submitter_id' => $user->id,
+        ]);
+
+        Organization::factory()->create([
+            'submitter_id' => $otherUser->id,
+        ]);
+
+        Site::factory()->create([
+            'submitter_id' => $user->id,
+        ]);
+
+        Site::factory()->create([
+            'organization_id' => $orgs[0]->id,
+            'submitter_id' => $user->id,
+        ]);
+
+        Site::factory()->create([
+            'submitter_id' => $otherUser->id,
         ]);
     }
 }
