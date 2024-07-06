d86430fb9de8a1c4b02b03e60925aecf3470caa7
Matt Stauffer <matt@tighten.co>
2024-07-05 23:35:59 -0400

    Tweak card hover state

--- resources/views/components/org-in-list.blade.php
+++ resources/views/components/org-in-list.blade.php
@@ -3,7 +3,7 @@
     x-on:click="window.location = link"
     onClick="window.location = this.getAttribute('link')"
     wire:key="org-{{ $org->id }}"
-    class="relative group bg-gray-300/20 backdrop-blur rounded-lg p-4 px-6 cursor-pointer shadow-card hover:shadow-card-blurred hover:bg-gray-100/30 transition">
+    class="relative group bg-gray-300/20 backdrop-blur-sm rounded-lg p-4 px-6 cursor-pointer shadow-card hover:shadow-card-blurred hover:bg-gray-100/40 transition">
     <h2 class="font-bold text-xl mb-4">
         <img src="{{ $org->favicon }}" alt="{{ $org->name }}" class="rounded-lg w-8 inline-block mr-2">
         {{ $org->name }} <a href="{{ $org->url }}">@include('icons.link')</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             boxShadow: {
-                'card': '0 0 40px -15px rgba(0, 0, 0, 0.2)',
-                'card-blurred': '0 0 60px -15px rgba(0, 0, 0, 0.15)',
+                'card': '0 0 20px -15px rgba(0, 0, 0, 0.2)',
+                'card-blurred': '0 0 15px -10px rgba(0, 0, 0, 0.35)',
             },
             backgroundImage: {
                 'site-light': "url('./images/temp-blueprint-bg.jpg)",
