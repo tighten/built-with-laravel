cee776d425406174186ae735968b8f124947ce80
Matt Stauffer <matt@tighten.co>
2024-07-08 16:20:54 -0400

    Tweak card padding

--- resources/views/components/org-in-list.blade.php
+++ resources/views/components/org-in-list.blade.php
@@ -3,12 +3,12 @@
     x-on:click="window.location = link"
     onClick="window.location = this.getAttribute('link')"
     wire:key="org-{{ $org->id }}"
-    class="relative group bg-black/4 backdrop-blur-sm rounded-lg py-4 px-6 cursor-pointer hover:bg-black/13 transition duration-300">
+    class="relative group bg-black/4 backdrop-blur-sm rounded-lg p-6 pt-5 cursor-pointer hover:bg-black/13 transition duration-300">
     <h2 class="font-bold text-xl mb-5">
         <img src="{{ $org->favicon }}" alt="{{ $org->name }}" class="rounded-lg w-9 inline-block mr-2">
         {{ $org->name }} <a href="{{ $org->url }}">@include('icons.link')</a>
     </h2>
-    <div class="mb-4 aspect-[600/444] rounded overflow-hidden relative border border-black/4">
+    <div class="aspect-[600/444] rounded overflow-hidden relative border border-black/4">
         <div class="w-full h-full absolute z-50 bottom-0"></div>
         <img alt="" width="540" height="400" class="rounded-sm aspect-[600/444] max-w-full group-hover:scale-115 transition duration-300 drop-shadow-[0_5px_5px_rgba(0,0,0,0.5)]" src="{{ $org->image }}">
     </div>
