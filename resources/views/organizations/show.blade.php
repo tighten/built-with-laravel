578beb56222e8a7b0886b5959f36b09fa61950e4
Matt Stauffer <matt@tighten.co>
2024-07-09 21:52:00 -0400

    Display notice if organization has no sites

--- resources/views/organizations/show.blade.php
+++ resources/views/organizations/show.blade.php
@@ -27,20 +27,20 @@
                 @endif
             </div>
 
-            @if ($organization->sites->count() > 0)
-                <div class="my-4">
+            <div class="my-4">
+                @if ($organization->sites->count() > 0)
                     @foreach ($organization->sites as $site)
                         <a href="{{ $site->url }}" class="block rounded-lg bg-black/4 p-4 py-2 backdrop-blur-sm mb-3 text-lg hover:bg-black/13 transition duration-300">
                             {{ $site->name }}
                             <span class="float-right mt-1"><img src="/images/chevron-forward.svg" alt=">"></span>
                         </a>
                     @endforeach
-                </div>
-            @else
-                TODO No sites using Laravel
-            @endif
-
-
+                @else
+                    <div class="p-2 text-bgrey-500">
+                        While this organization is known to use Laravel, we don't currently have links to any publicly-accessible sites or apps known to be using Laravel.
+                    </div>
+                @endif
+            </div>
         </div>
 
         <div class="col-span-3 mt-8 md:mt-0">
