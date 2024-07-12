0300391623bf067b844e349801f45ab59b5079e5
Matt Stauffer <matt@tighten.co>
2024-07-12 10:20:47 -0400

    Add favicon

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Built with Laravel</title>
+        <link rel="icon" href="/images/favicon.ico">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
