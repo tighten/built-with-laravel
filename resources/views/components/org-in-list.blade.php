c315e2fabf0e19aa53821bbc153efc8dfc59742b
Tony Messias <tonysm@hey.com>
2024-07-12 18:48:29 -0300

    Use slugs for the view transitions

--- resources/views/components/org-in-list.blade.php
+++ resources/views/components/org-in-list.blade.php
@@ -1,9 +1,4 @@
-<div class="group relative cursor-pointer rounded-lg bg-black/4 p-4 backdrop-blur-lg transition duration-300 hover:bg-black/13 md:p-6 md:pt-5">
-    <h2 class="mb-5 text-xl font-bold" style="view-transition-name: organization-{{ $org->id }};">
-        <img loading="lazy" src="{{ $org->favicon }}" alt="{{ $org->name }}" class="mr-2 inline-block w-9 rounded-lg" />
-        <a href="{{ route('organizations.show', $org) }}">{{ $org->name }}<span class="absolute inset-0 z-10"></span></a>
-    </h2>
-
+<div class="flex flex-col-reverse group relative cursor-pointer rounded-lg bg-black/4 p-4 backdrop-blur-lg transition duration-300 hover:bg-black/13 md:p-6 md:pt-5">
     <div class="aspect-[600/444] overflow-hidden rounded border border-black/4">
         @if ($org->sites->count() > 0)
             <img
@@ -13,7 +8,7 @@
                 height="400"
                 class="aspect-[600/444] max-w-full rounded-sm drop-shadow-[0_5px_5px_rgba(0,0,0,0.5)] transition duration-300 group-hover:scale-115"
                 src="{{ $org->sites->first()->image }}"
-                style="view-transition-name: main-site-{{ $org->sites->first()->id }}"
+                style="view-transition-name: main-site-{{ $org->sites->first()->slug }}"
             />
         @else
             <div
@@ -27,9 +22,15 @@
                     height="400"
                     class="aspect-[600/444] max-w-full rounded-sm transition duration-300 group-hover:scale-115"
                     src="{{ $org->image }}"
-                    style="view-transition-name: no-site-{{ $org->id }}"
+                    style="view-transition-name: no-site-{{ $org->slug }}"
                 />
             </div>
         @endif
     </div>
+
+    {{-- This is at the bottom because the image wouldn't be clickable otherwise (this is flipped via flexbox)... --}}
+    <h2 class="mb-5 text-xl font-bold" style="view-transition-name: organization-{{ $org->slug }};">
+        <img loading="lazy" src="{{ $org->favicon }}" alt="{{ $org->name }}" class="mr-2 inline-block w-9 rounded-lg" />
+        <a href="{{ route('organizations.show', $org) }}">{{ $org->name }}<span class="absolute inset-0 z-10"></span></a>
+    </h2>
 </div>
--- resources/views/organizations/show.blade.php
+++ resources/views/organizations/show.blade.php
@@ -2,7 +2,7 @@
     <div class="grid-cols-3 gap-10 lg:grid xl:grid-cols-4">
         <div>
             <div class="rounded-xl bg-black/4 p-4 backdrop-blur-lg">
-                <h2 class="mb-3 text-xl font-bold" style="view-transition-name: organization-{{ $organization->id }};">
+                <h2 class="mb-3 text-xl font-bold" style="view-transition-name: organization-{{ $organization->slug }};">
                     <img
                         loading="lazy"
                         src="{{ $organization->favicon }}"
@@ -57,10 +57,10 @@
 
         <div class="col-span-2 mt-8 md:mt-0 xl:col-span-3">
             @if ($organization->sites->count() === 0)
-                <img style="view-transition-name: no-site-{{ $organization->id }}" loading="lazy" src="{{ $organization->image }}" class="rounded-md border" />
+                <img style="view-transition-name: no-site-{{ $organization->slug }}" loading="lazy" src="{{ $organization->image }}" class="rounded-md border" />
             @else
                 @foreach ($organization->sites as $site)
-                    <div id="site-{{ $site->slug }}" class="mb-10" style="view-transition-name: main-site-{{ $site->id }}">
+                    <div id="site-{{ $site->slug }}" class="mb-10" style="view-transition-name: main-site-{{ $site->slug }}">
                         <div class="font-bold">{{ $site->name }}</div>
                         <div class="relative">
                             <a
