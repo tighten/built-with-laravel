df9da3563860b1a65bc9fd65221bacbafe271965
Matt Stauffer <matt@tighten.co>
2024-07-22 21:56:17 -0400

    Manually spin up Alpine if Livewire isn't present

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,13 @@
 import axios from 'axios';
+import Alpine from 'alpinejs';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+if (typeof(window.Alpine) === 'undefined') {
+    // Manually start up Alpine so it works on cached pages when Livewire doesn't load
+    window.Alpine = Alpine;
+
+    Alpine.start();
+}
