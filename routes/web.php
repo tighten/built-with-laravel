ba06c883e0263004bb5f1fdc032f8d3a9ebe7191
Matt Stauffer <matt@tighten.co>
2024-08-06 13:02:14 -0400

    Fix login redirect route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\SuggestionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('login', 'bts/login')->name('login');
+
 Route::resource('organizations', OrganizationController::class)->only('show')->middleware('page-cache');
 Route::resource('suggestions', SuggestionsController::class)->only(['create', 'store']);
 Route::get('{technology:slug?}', [OrganizationController::class, 'index'])->name('home')->middleware('page-cache');
