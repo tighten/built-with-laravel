9db7dc7eb4f8c4876bfb29775ab790fda2c46a15
Tony Messias <tonysm@hey.com>
2024-08-10 00:58:46 -0300

    Remove Bugsnag

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\BtsPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
