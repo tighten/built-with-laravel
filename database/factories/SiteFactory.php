51957dc766554a71c4e5c8a7a0aa3ed7c3739bf9
Shift <shift@laravelshift.com>
2026-03-10 14:00:15 +0000

    Shift cleanup

--- database/factories/SiteFactory.php
+++ database/factories/SiteFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Organization;
-use App\Models\Site;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SiteFactory extends Factory
--- database/factories/SuggestedOrganizationFactory.php
+++ database/factories/SuggestedOrganizationFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\SuggestedOrganization;
 use App\Models\Technology;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
